988180be7dfdb0b47944d7d8c8531da780f17761
harshpawar12 <harshadpawar68336@gmail.com>
2024-09-29 01:36:18 +0530

    StringBuilder

---
+++ CompareTo.java
@@ -0,0 +1,27 @@
+package StringBuffer;
+
+public class CompareTo {
+	CompareTo(){
+	StringBuffer s1=new StringBuffer("harsh");
+	StringBuffer s2=new StringBuffer("nitin");
+
+	if(s1.equals(s1))//equals method in string buffer
+					//is used to check the refernce of object
+	{
+		System.out.println("both are equval");
+	}
+	else
+	{
+		System.out.println("both are not equval");
+	}
+	
+	
+		
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		CompareTo h=new CompareTo();
+
+	}
+
+}
